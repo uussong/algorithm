7b3d155df5368a9d27d13d3a4a08d708f39b2407
ssong <77879633+uussong@users.noreply.github.com>
2022-03-02 01:35:51 +0900

    [Bronze III] Title: TGN, Time: 76 ms, Memory: 30860 KB -BaekjoonHub

---
+++ 백준/Bronze/5063.TGN/TGN.py
@@ -0,0 +1,10 @@
+N = int(input())
+
+for _ in range(N):
+    r, e, c = map(int, input().split())
+    if e - c > r:
+        print('advertise')
+    elif e - c == r:
+        print('does not matter')
+    elif e - c < r:
+        print('do not advertise')
