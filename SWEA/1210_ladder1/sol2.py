5ac0908a093bbe75f582cbe489802d9584c7e365
uussong <yoosong0713@gmail.com>
2022-03-12 13:41:48 +0900

    1210_ladder1

--- SWEA/1210_ladder1/sol2.py
+++ SWEA/1210_ladder1/sol2.py
@@ -6,26 +6,24 @@ T = 10
 for tc in range(1, T + 1):
     n = int(input())
     ladder = [list(map(int, input().split())) for _ in range(100)]
-    r = 99
-    for i in range(100):
-        if ladder[99][i] == 2:
-            c = i
 
-    d_row = [-1, 0, 0]
-    d_col = [0, -1, 1]
+    r = 99
+    for c in range(100):
+        if ladder[r][c] == 2:
+            break
+
+    dr = [-1, 0, 0]
+    dc = [0, -1, 1]
     d = 0
 
     while r > 0:
-        if d == 0:
-            if c > 0 and ladder[r][c - 1] == 1:
-                d = 1
-            elif c < 99 and ladder[r][c + 1] == 1:
-                d = 2
-        else:
-            if ladder[r - 1][c] == 1:
-                d = 0
+        nr = r + dr[d]
+        nc = c + dc[d]
+        if 0 <= nr < 100 and 0 <= nc < 100:
+            if ladder[nr][nc] == 1:
+                r = nr
+                c = nc
+                ladder[nr][nc] = 0
+        d = (d+1) % 3
 
-        r += d_row[d]
-        c += d_col[d]
     print(f'#{tc} {c}')
-
