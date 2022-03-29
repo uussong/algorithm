31cfa30e6831f0e338b60fbb7c4886ec074a3c76
uussong <yoosong0713@gmail.com>
2022-03-30 00:21:50 +0900

    5188_최소합

---
+++ SWEA/5188_최소합/sol1.py
@@ -0,0 +1,25 @@
+import sys
+
+sys.stdin = open('sample_input.txt')
+
+def dfs(x, y, acc):
+    global result
+
+    if x == N-1 and y == N-1:   # 종료
+        result = min(acc, result)
+    else:
+        if x + 1 < N:   # 아래로 움직이는 경우
+            dfs(x + 1, y, acc + arr[x + 1][y])
+        if y + 1 < N:   # 오른쪽으로 움직이는 경우
+            dfs(x, y + 1, acc + arr[x][y + 1])
+
+
+T = int(input())
+for tc in range(1, T + 1):
+    N = int(input())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    result = 999999
+
+    dfs(0, 0, arr[0][0])
+    print(f'#{tc} {result}')
+
