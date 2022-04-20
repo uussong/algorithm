3da1cd39012aa7a10a3f9e5e6d8edd4aceded105
uussong <yoosong0713@gmail.com>
2022-04-20 23:42:49 +0900

    1206_view

--- SWEA/1206_view/sol2.py
+++ SWEA/1206_view/sol2.py
@@ -6,10 +6,15 @@ T = 10
 for tc in range(1, T + 1):
     N = int(input())
     buildings = list(map(int, input().split()))
-    view = 0
+    ans = 0
+    for i in range(2, N-2):
+        now = buildings[i]
+        left = max(buildings[i-2:i])    # 인덱스 슬라이싱 이용
+        right = max(buildings[i+1:i+3])
+        
+        # 현재 빌딩이 왼쪽, 오른쪽 모든 빌딩보다 높아야 조망권 확보
+        if now > left and now > right:
+            ans += now - max(left, right)
 
-    for i in range(2, N-1):
-        if buildings[i] > buildings[i-1] and buildings[i] > buildings[i-2] and buildings[i] > buildings[i+1] and buildings[i] > buildings[i+2]:
-            view += min(buildings[i]-buildings[i-1], buildings[i]-buildings[i-2], buildings[i]-buildings[i+1], buildings[i]-buildings[i+2])
-    print(f'#{tc} {view}')
+    print(f'#{tc} {ans}')
 
