daa83448e253875f840d0e0b8ec26e8e9c223fba
uussong <77879633+uussong@users.noreply.github.com>
2023-05-06 13:09:57 +0900

    [level 0] Title: 팩토리얼, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    let num = 1
+    for(let i = 1; i <= n; i++) {
+        num *= i
+        if(num === n) {
+            return i
+        } else if (num > n) {
+            return i - 1
+        }
+    }
+    
+}
