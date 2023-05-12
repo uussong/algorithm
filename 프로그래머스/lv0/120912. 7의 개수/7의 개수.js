4983db06079f5fa7c8e9da7bcec36639d7290b15
uussong <77879633+uussong@users.noreply.github.com>
2023-05-13 08:51:31 +0900

    [level 0] Title: 7의 개수, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
@@ -1,12 +1,9 @@
 function solution(array) {
     let result = 0
     array = array.map(item => item.toString())
-    for(const item of array) {
-        for(const num of item) {
-            if(num === '7') {
-                result += 1
-            }
-        }
+    result = array.join('').match(/7/g)
+    if (result === null) {
+        return 0
     }
-    return result
+    return result.length
 }
