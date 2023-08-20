3a02489d6e927ea1ef35df448928b1aa2c721a4e
uussong <77879633+uussong@users.noreply.github.com>
2023-08-20 23:26:50 +0900

    [level 1] Title: 예산, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -0,0 +1,11 @@
+function solution(d, budget) {
+    let answer = 0;
+    d.sort((a, b) => a - b)
+
+    for (let i = 0; i < d.length; i++) {
+        budget -= d[i]
+        if (budget < 0) break
+        answer += 1
+    }
+    return answer;
+}
